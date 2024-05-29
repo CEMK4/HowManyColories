7fa0f8b3c9e39765276525baffdc727dd86c2255
CEMK4 <ceme4k4@mail.ru>
2024-05-29 14:52:56 +0300

    Initial commit

---
+++ src/main/java/com/HowManyColories/HowManyColories/HowManyColoriesApplication.java
@@ -0,0 +1,13 @@
+package com.HowManyColories.HowManyColories;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowManyColoriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HowManyColoriesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/HowManyColories/HowManyColories/HowManyColoriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HowManyColories.HowManyColories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowManyColoriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
